ef72eaa0d7e37e12c6f686de0f396f62261075df
ilDug <dognini.marco@gmail.com>
2023-12-01 17:29:57 +0100

    creating docker-compose and ENV VAR

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
 FROM alpine:3
 
+RUN apk add --no-cache 	mongodb-tools
+
 ADD cmd/entry.sh /entry.sh
 COPY cmd/script.sh /script.sh
 RUN chmod 755 /script.sh /entry.sh
-# RUN /usr/bin/crontab /crontab.txt
 
 ENV CRON="* * * * *"
+ENV MONGO_ROOT_PW_FILE=/run/secrets/MONGO_ROOT_PW
+ENV USER=root
+ENV HOST=db
+ENV PORT=27017
+ENV DB=admin
 
 ENTRYPOINT  ["/entry.sh"]
--- cmd/script.sh
+++ cmd/script.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-# code goes here.
-echo "This is a script, run by cron!"
+# PW=$(echo $MONGO_ROOT_PW_FILE)
+PW="cippa"
+
+# connection string
+CS="mongodb://$USER:$PW@$HOST:$PORT/$DB?authSource=admin"
+
+echo $CS
+
+
+# montare una cartella sul server dove salvare il BACKUP
+
+# echo $(date) >>/backup/backup.$(date +%Y.%m).log
+
+# docker exec mongodb sh -c "mongodump -v  -u root -p $MONGO_ROOT_PW --authenticationDatabase admin --out=/backup --db=ek" 2>&1 | tee -a /backup/backup.$(date +%Y.%m).log
+
+# echo $? >>/backup/backup.$(date +%Y.%m).log
+####################################################################################
+# docker exec mongodb sh -c "mongorestore -v -u root -p $MONGO_ROOT_PW --authenticationDatabase admin --dir=/backup --drop"
